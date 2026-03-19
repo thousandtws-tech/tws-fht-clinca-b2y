18b859f7d5dc6a5134e7dbcf0900b9f4e67f6f57
hebcodee <herberth.ferreira6@gmail.com>
2026-03-19 14:27:49 -0300

    feat: complete attributes entitys

--- org.tws.by2/apps/server/src/main/java/com/tws/company/domain/Appointments.java
+++ org.tws.by2/apps/server/src/main/java/com/tws/company/domain/Appointments.java
@@ -1,4 +1,25 @@
 package com.tws.company.domain;
 
+import com.tws.company.domain.enums.UserStatus;
+import com.tws.company.domain.enums.UserType;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Appointments {
+    @Id
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    @Column("id")
+
+    String patientId;
+    String patientName;
+    String doctorId;
+    String doctorName;
+    List<String>  specialty;
+    UserType type;
+    LocalDate appointmentDate;
+    UserStatus status;
+    String telemedicineRoomUrl;
+    String aiAnalysisReport;
+    String createdBy;
 }
--- org.tws.by2/apps/server/src/main/java/com/tws/company/domain/DoctorTimeSlots.java
+++ org.tws.by2/apps/server/src/main/java/com/tws/company/domain/DoctorTimeSlots.java
@@ -1,4 +1,30 @@
 package com.tws.company.domain;
 
+import com.tws.company.domain.enums.UserStatus;
+import com.tws.company.domain.enums.UserType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public class DoctorTimeSlots {
+    @Id
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    @Column("id")
+    private String id;
+
+    String doctorId;
+    String doctorName;
+    LocalDate date;
+    LocalTime startTime;
+    LocalTime endTime;
+    Boolean isOvernight;
+    UserType serviceType;
+    List<String>  specialties;
+    BigDecimal desiredHourlyRate;
+    UserStatus status;
+    String notes;
+    List<String> cities;
+    List<String> state;
 }
